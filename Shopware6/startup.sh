f15a9c42d47f0c2c304fbb3b5fae4ccb76f8be0b
Simon Prinz <Hey@SimonPri.nz>
2021-11-19 09:30:35 +0100

    create install lockfile when the installation is finished

--- Shopware6/startup.sh
+++ Shopware6/startup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [ ! -e /shopware/inst.lock ]; then
-  touch /shopware/inst.lock
   echo 'setting shopware up'
   php /shopware/bin/console system:setup -nq \
     --database-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:3306/$MYSQL_DATABASE \
     --generate-jwt-keys
   echo 'installing shopware'
   php /shopware/bin/console system:install -fnq
+  touch /shopware/inst.lock
 fi
 
 exec "$@"
